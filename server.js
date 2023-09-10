328b9b6a22ed9715a229e2fd8d72a4149718debe
Wendy Zhu <wendyy.zhu@hotmail.com>
2023-09-10 23:20:27 +1000

    add api endpoint for all stations

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ app.get('/', (req, res) => {
     res.send('Weather stations API')
 })
 
+app.get('/api/stations/all', (req, res) => {
+    const sql = `SELECT * FROM stations ORDER BY id;`
+    db
+        .query(sql)
+        .then(res => res.rows)
+        .then(stations => res.json(stations))
+})
+
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
 })
