bc0f7e6578d2d79a1053270621a5bd10b64269ed
Wendy Zhu <wendyy.zhu@hotmail.com>
2023-09-12 13:56:22 +1000

    redirect all unmatched endpoints to /

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const app = express(); 
 const port = process.env.PORT || 4000;
-const db = require('./database/index');
 const stationsRouter = require('./routes/stations')
 const variablesRouter = require('./routes/variables')
 const dataRouter = require('./routes/data')
@@ -16,6 +15,10 @@ app.use('/api/stations', stationsRouter)
 app.use('/api/variables', variablesRouter)
 
 app.use('/api/data', dataRouter)
+
+app.get('*', (req, res) => {
+    res.redirect('/')
+})
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
